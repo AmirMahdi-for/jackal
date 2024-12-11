deca8321757f2600265253a862a9a24c0219193e
AmirMahdi-for <amirmahdifor@gmail.com>
2024-12-11 08:57:09 -0800

    feat: write unit test for get entry data

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/amirmahdi-for/jackal/util"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 func createRandomEntry(t *testing.T, account Accounts) Entries {
@@ -29,4 +30,17 @@ func createRandomEntry(t *testing.T, account Accounts) Entries {
 func TestCreateEntry(t *testing.T) {
 	account := createRandomAccount(t)
 	createRandomEntry(t, account)
+}
+
+func TestGetEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	entry1 := createRandomEntry(t, account)
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
 }
