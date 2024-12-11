9356a21f80d72024482894e08a1a33651516d8ae
AmirMahdi-for <amirmahdifor@gmail.com>
2024-12-11 09:35:54 -0800

    last changes for transfers. with BUGS

--- db/sqlc/models.go
+++ db/sqlc/models.go
@@ -25,7 +25,7 @@ type Entries struct {
 type Transfers struct {
 	ID            int64         `json:"id"`
 	FromAccountID int64         `json:"from_account_id"`
-	ToAccountID   sql.NullInt64 `json:"to_account_id"`
+	ToAccountID   int64 		`json:"to_account_id"`
 	Amount        int64         `json:"amount"`
 	CreatedAt     time.Time     `json:"created_at"`
 }
